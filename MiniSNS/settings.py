03da2545da5403ccf23ec1601078b1a0666f0317
Busho100 <knight-pine@i.softbank.jp>
2022-12-12 11:19:25 +0900

    デプロイの準備

--- MiniSNS/settings.py
+++ MiniSNS/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [] #デプロイ前　先コードを[]内に記入　”'127.0.0.1', '.pythonanywhere.com', 'hanahana.pythonanywhere.com（pythonanywhereのアドレス']”
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com'] #デプロイ前　先コードを[]内に記入　”'127.0.0.1', '.pythonanywhere.com', 'hanahana.pythonanywhere.com（pythonanywhereのアドレス']”
 
 
 # Application definition
